b20cdbcfac1642d22111828f169e586a29c51b8b
jetz <admin@kimjunyoung.com>
2024-01-30 17:19:53 +0900

    Initial commit

---
+++ src/main/java/jetzengine/JetzEngineApplication.java
@@ -0,0 +1,13 @@
+package jetzengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetzEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JetzEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jetzengine/JetzEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package jetzengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetzEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
