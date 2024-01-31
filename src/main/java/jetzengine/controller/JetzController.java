0309813b0d7456cde8967ef6a3f82c805291ff7a
jetz <admin@kimjunyoung.com>
2024-01-31 14:14:41 +0900

    [Z-1]유저 도메인 개발
    feat: 스프링 시큐리티 적용

---
+++ src/main/java/jetzengine/controller/JetzController.java
@@ -0,0 +1,13 @@
+package jetzengine.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class JetzController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
