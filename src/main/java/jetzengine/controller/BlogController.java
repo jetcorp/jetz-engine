96ee4637a526d8b0dbefff49dba84a7cca5f6f3c
jetz <admin@kimjunyoung.com>
2024-02-06 10:50:26 +0900

    [Z-2]블로그 도메인 개발
    feat: "내 블로그" 링크 추가

--- src/main/java/jetzengine/controller/BlogController.java
+++ src/main/java/jetzengine/controller/BlogController.java
@@ -2,7 +2,9 @@ package jetzengine.controller;
 
 import java.security.Principal;
 import jetzengine.entity.Blog;
+import jetzengine.entity.Zuser;
 import jetzengine.service.BlogService;
+import jetzengine.service.ZuserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,11 +15,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RequiredArgsConstructor
 public class BlogController {
     private final BlogService blogService;
+    private final ZuserService zuserService;
 
     @GetMapping("/@{zname}")
     public String userBlog(@PathVariable String zname, Model model, Principal principal) {
         if (blogService.findBlog(zname) == null) {
             blogService.create(zname);
+        }
+
+        if (principal != null) {
+            Zuser zuser = zuserService.getZuser(principal.getName());
+            model.addAttribute("zuser", zuser);
         }
 
         Blog blog = blogService.findBlog(zname);
--- src/main/java/jetzengine/controller/JetzController.java
+++ src/main/java/jetzengine/controller/JetzController.java
@@ -1,13 +1,25 @@
 package jetzengine.controller;
 
+import java.security.Principal;
+import jetzengine.entity.Zuser;
+import jetzengine.service.ZuserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
 public class JetzController {
+    private final ZuserService zuserService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model, Principal principal) {
+        if (principal != null) {
+            Zuser zuser = zuserService.getZuser(principal.getName());
+            model.addAttribute("zuser", zuser);
+        }
+
         return "index";
     }
 }
